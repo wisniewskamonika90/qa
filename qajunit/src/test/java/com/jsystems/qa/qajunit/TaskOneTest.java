e9fbf415b177624b3097be2c3f67ebb9bfa3a83f
wisniewskamonika90 <wisniewska.monika90@gmail.com>
2019-11-19 12:18:16 +0100

    zadanie

---
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/TaskOneTest.java
@@ -0,0 +1,27 @@
+package com.jsystems.qa.qajunit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TaskOneTest {
+
+
+    @Test
+    public void zad1(){
+        String resultString = new String("result of test 154% success");
+        String expectedString = new String ("result od test [number]% success");
+
+        assertTrue(resultString.startsWith("result of test"));
+        assertTrue(resultString.endsWith("success"));
+        assertThat(resultString).matches("(result of test )\\d+(% success)");
+
+        String result = resultString.replace("result of test ", "").replace("% success", "");
+        System.out.println(result);
+        int resultNumber = Integer.parseInt(result);
+        assertTrue(resultNumber >0);
+    }
+}
